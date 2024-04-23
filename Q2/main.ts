aa7d6207507d12b89776df37a6afc9052c11fa2d
Hassan0026 <159889462+Hassan0026@users.noreply.github.com>
2024-04-23 22:35:21 +0500

    Q2

---
+++ Q2/main.ts
@@ -0,0 +1,6 @@
+//    2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+// would you like to learn some Python today?”
+
+
+let userName: string = "Hassan Jamshed";
+console.log('Hello ${userName}, would you like to learn some Python today?');
